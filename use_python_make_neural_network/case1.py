95df13ed996016dde43626eb25acee221e3d3c5b
r00txx <82049406@qq.com>
2018-05-31 18:48:53 +0800

    tactx

---
+++ use_python_make_neural_network/case1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#Author: r00txx
+
+def calArea(radius):
+  area = 3.14 * radius * radius
+
+
+def learn(model, data):
+  '''
+  通过数据data训练模型model的代码
+  '''
+
+  #返回已经训练好的模型
+  return model
+
+
+def test(model, x):
+  '''
+  将 x 输入模型 model 中得出结果 y 的代码
+  '''
+
+  #返回计算结果 ---- 圆的面积
+  return y
+
+
+
+if __name__ == '__main_-':
+  #通过训练得到模型 trained_model
+  trained_model = learn(init_model, data)
+  #得到半路为100的圆的面积
+  y = test(trained_model, x = 100)
+
+
+
+
+
