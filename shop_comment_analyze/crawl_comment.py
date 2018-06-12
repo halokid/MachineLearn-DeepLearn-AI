980d6565e16e43347e1ca98d812c7e3718acf393
r00txx <82049406@qq.com>
2018-06-12 18:28:06 +0800

    tactx

---
+++ shop_comment_analyze/crawl_comment.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# defaultencoding = 'utf-8'
+
+"""
+爬取美团的商家评论页面评论内容
+"""
+
+import json
+import requests
+import random
+import csv
+import logging; logging.basicConfig(level=logging.INFO)
+
+
+
